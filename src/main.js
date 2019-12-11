f79a6d7562b536299f68693bfba8387220cd9b6c
Maximov1233 <52001281+Maximov1233@users.noreply.github.com>
2019-12-11 15:36:55 +0600

    adaptive prepare

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
-console.log(document.documentElement.clientHeight);
+const carouselSlide  = document.querySelector('.carousel-slide'),
+      carouselImages = document.querySelectorAll('.carousel-slide img'),
+      prevBtn        = document.querySelector('.prevBtn'),
+      nextBtn        = document.querySelector('.nextBtn'),
+      size           = carouselImages[0].clientWidth;
+
+let counter = 1;
+
+carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+    counter++;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+    counter--;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImages[counter].id === 'last') {
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length -2;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    } else if (carouselImages[counter].id === 'first') {
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length - counter;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    }
+});
+
