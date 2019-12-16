bb468325f3efd1e41747eb1664dbf1bfb609bc1f
Maximov1233 <52001281+Maximov1233@users.noreply.github.com>
2019-12-16 21:00:28 +0600

    scrolling

--- src/main.js
+++ src/main.js
@@ -1,36 +1,80 @@
-const carouselSlide  = document.querySelector('.carousel-slide'),
-      carouselImages = document.querySelectorAll('.carousel-slide img'),
-      prevBtn        = document.querySelector('.prevBtn'),
-      nextBtn        = document.querySelector('.nextBtn'),
-      size           = carouselImages[0].clientWidth;
+// slider
+
+const reviewWindow = document.querySelector('.reviews-list'),
+      reviews      = document.querySelectorAll('.review-window .review'),
+      prevBtn      = document.querySelector('.left-arrow'),
+      nextBtn      = document.querySelector('.right-arrow'),
+      size         = reviews[0].clientWidth;
 
 let counter = 1;
 
-carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+//reviewWindow.style.transform = `translateX(${-size * counter}px)`;
 
 nextBtn.addEventListener('click', () => {
-    if (counter >= carouselImages.length - 1) return;
-    carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+    
+    // if (counter >= reviews.length) return;
+    reviewWindow.style.transition = `transform 0.4s ease-in-out`; 
+    
+    reviewWindow.style.transform = `translateX(${-size * counter}px)`;
+    reviews.forEach((review) => {
+        review.style.zIndex = ``;
+    });
+    reviews[counter].style.zIndex = `99`;
     counter++;
-    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
 });
 
 prevBtn.addEventListener('click', () => {
-    if (counter <= 0) return;
-    carouselSlide.style.transition = `transform 0.4s ease-in-out`;
+    // if (counter <= 0) return;
+    reviewWindow.style.transition = `transform 0.4s ease-in-out`;
     counter--;
-    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    reviewWindow.style.transform = `translateX(${size * (-counter + 1)}px)`;
+    reviews.forEach((review) => {
+        review.style.zIndex = ``;
+    });
+    reviews[counter - 1].style.zIndex = `99`; 
 });
 
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'last') {
-        carouselSlide.style.transition = 'none';
-        counter = carouselImages.length -2;
-        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
-    } else if (carouselImages[counter].id === 'first') {
-        carouselSlide.style.transition = 'none';
-        counter = carouselImages.length - counter;
-        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
-    }
+
+// reviewWindow.addEventListener('transitionend', () => {
+    
+//     if (reviews[counter].id === 'last') {
+//         reviewWindow.style.transition = 'none';
+//         counter = reviews.length -2;
+//         reviewWindow.style.transform = `translateX(${-size * counter}px)`;
+//     } else if (reviews[counter].id === 'first') {
+//         reviewWindow.style.transition = 'none';
+//         counter = reviews.length - counter;
+//         reviewWindow.style.transform = `translateX(${-size * counter}px)`;
+//     }
+// });
+
+// modal close
+
+const modalBg = document.querySelector('.header-modal__bg'),
+      modal = modalBg.querySelector('.header-modal'),
+      modalClose = modal.querySelector('.modal-close'),
+      modalOpeners = document.querySelectorAll('.modal-opener');
+
+modalOpeners.forEach((opener) => {
+    opener.addEventListener('click', () => {
+        document.body.classList.add('ov-h');
+        modalBg.classList.remove('hide'); 
+    });
 });
+
+modalClose.addEventListener('click', () => {
+    modalBg.classList.add('hide');
+    document.body.classList.remove('ov-h');
+});
+
+modalBg.addEventListener('click', () => {
+    modalBg.classList.add('hide');
+    document.body.classList.remove('ov-h');
+});
+
+const scroll = new SmoothScroll('.header-nav__item a' ,  {
+    speed: 400
+});
+
+    
 
